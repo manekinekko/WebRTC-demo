f6b82b9f8689ddcb55137012b7c01ddeef41bb67
manekinekko <maneki.nekko@gmail.com>
2014-02-23 15:19:38 +0100

    First import

---
+++ client/js/webrtc.js
@@ -0,0 +1,42 @@
+﻿!function(window, document, $, undefined) {
+
+	var socket = io.connect('http://localhost:8888');
+	var getUserMedia =
+		navigator.getUserMedia ||
+		navigator.webkitGetUserMedia ||
+		navigator.MozGetUserMedia ||
+		navigator.oGetUserMedia ||
+		navigator.msGetUserMedia;
+		
+	$(function() {
+		var video = document.getElementById('client-video');
+		var receive = $('#receive');
+		var canvas = document.getElementById('remote-video');
+			
+		socket.emit('hello', { hello: 'world' });
+		socket.on('capture', function(data) {
+			receive.attr('src', data);
+		});
+
+		if (!getUserMedia) {
+			// alert('your browser unsupported webrtc.');
+			return false;
+		}
+				
+		navigator.webkitGetUserMedia('video audio', function(stream) {
+			video.src = webkitURL.createObjectURL(stream);
+			video.addEventListener('error', function () {
+				stream.stop();
+			});
+		});
+		
+		video.addEventListener('timeupdate', function(e) {
+			var ctx = canvas.getContext('2d');
+			
+			ctx.drawImage(video, -video.width/2, -video.height/2);
+		});
+		
+		socket.emit('capture', canvas.toDataURL());
+	});
+	
+}(window, document, jQuery);
---
+++ server/webrtc.js
@@ -0,0 +1,15 @@
+﻿var http = require('http');
+var server = http.createServer();
+var app = server.listen(8888);
+var io = require('socket.io').listen(app);
+
+var _clientID;
+io.sockets.on('connection', function(client) {
+	
+	client.on('capture', function(data) {
+		io.sockets.emit('capture', data);
+	});
+	client.on('hello', function(data) {
+	});
+	
+});
